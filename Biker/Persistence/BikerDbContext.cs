5b98574e688266d0a526d26ab335aeedb399425c
nenadnesta <nenadnesta@gmail.com>
2019-11-15 20:18:59 +0100

    DbContext - Creating.

---
+++ Biker/Persistence/BikerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Biker.Persistence
+{
+    public class BikerDbContext : DbContext
+    {
+        public BikerDbContext(DbContextOptions<BikerDbContext> options)
+             : base(options)
+        {
+
+        }
+    }
+}
