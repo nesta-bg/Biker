48ec20ca3df524c95464e8ffebf213691dd88d41
nenadnesta <nenadnesta@gmail.com>
2019-11-15 20:45:03 +0100

    Applying Constraints To Domain Models.

--- Biker/Models/Make.cs
+++ Biker/Models/Make.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Biker.Models
 {
@@ -7,6 +8,8 @@ namespace Biker.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public ICollection<Model> Models { get; set; }
--- Biker/Models/Model.cs
+++ Biker/Models/Model.cs
@@ -1,9 +1,15 @@
-﻿namespace Biker.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Biker.Models
 {
+    [Table("Models")]
     public class Model
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public Make Make { get; set; }
