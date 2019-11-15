5001fe917625a64db8fbddf88b54ae8ff5bb1a62
nenadnesta <nenadnesta@gmail.com>
2019-11-15 20:03:22 +0100

    Make and Model - Creating Domain Models.

---
+++ Biker/Models/Model.cs
@@ -0,0 +1,13 @@
+﻿namespace Biker.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Make Make { get; set; }
+
+        public int MakeId { get; set; }
+    }
+}
