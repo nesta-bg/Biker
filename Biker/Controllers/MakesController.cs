dd39a7292d5a7ff35d501362768433f9ace019ec
nenadnesta <nenadnesta@gmail.com>
2019-11-16 11:59:03 +0100

    MakesController - Building API.

---
+++ Biker/Controllers/MakesController.cs
@@ -0,0 +1,25 @@
+﻿using Biker.Models;
+using Biker.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Biker.Controllers
+{
+    public class MakesController : Controller
+    {
+        private readonly BikerDbContext context;
+
+        public MakesController(BikerDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<Make>> GetMakes()
+        {
+            return await context.Makes.Include(m => m.Models).ToListAsync();
+        }
+    }
+}
