5155e0c6192300853614609d46973da5fa58818c
nenadnesta <nenadnesta@gmail.com>
2019-11-16 12:31:21 +0100

    Adding AutoMapper.

--- Biker/Controllers/MakesController.cs
+++ Biker/Controllers/MakesController.cs
@@ -1,4 +1,6 @@
-﻿using Biker.Models;
+﻿using AutoMapper;
+using Biker.Controllers.Resources;
+using Biker.Models;
 using Biker.Persistence;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,16 +12,22 @@ namespace Biker.Controllers
     public class MakesController : Controller
     {
         private readonly BikerDbContext context;
+        private readonly IMapper mapper;
 
-        public MakesController(BikerDbContext context)
+        public MakesController(BikerDbContext context, IMapper mapper)
         {
             this.context = context;
+            this.mapper = mapper;
         }
 
         [HttpGet("/api/makes")]
-        public async Task<IEnumerable<Make>> GetMakes()
+        public async Task<IEnumerable<MakeResource>> GetMakes()
         {
-            return await context.Makes.Include(m => m.Models).ToListAsync();
+            var makes = await context.Makes.Include(m => m.Models).ToListAsync();
+
+            return mapper.Map<List<Make>, List<MakeResource>>(makes);
+
         }
     }
 }
+
---
+++ Biker/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Biker.Controllers.Resources;
+using Biker.Models;
+
+namespace Biker.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Make, MakeResource>();
+            CreateMap<Model, ModelResource>();
+        }
+    }
+}
