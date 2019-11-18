ba643d3ea78d5aa93eb554d9a90518b64c7cbda0
nenadnesta <nenadnesta@gmail.com>
2019-11-18 19:55:33 +0100

    BikeForm - Building Bootstrap Form.

--- Biker/ClientApp/src/app/bike-form/bike-form.component.ts
+++ Biker/ClientApp/src/app/bike-form/bike-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-bike-form',
   templateUrl: './bike-form.component.html',
   styleUrls: ['./bike-form.component.css']
 })
