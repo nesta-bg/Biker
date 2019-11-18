0512280c76f7b5dfe121c75247213c4657c6341f
nenadnesta <nenadnesta@gmail.com>
2019-11-18 14:38:05 +0100

    NavMenu Component.

--- Biker/ClientApp/src/app/app.module.ts
+++ Biker/ClientApp/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { NavMenuComponent } from './navmenu/navmenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ Biker/ClientApp/src/app/navmenu/navmenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nav-menu',
+  templateUrl: './navmenu.component.html',
+  styleUrls: ['./navmenu.component.css']
+})
+export class NavMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
