b9ca906447f769109c87464b0653d03517adf1b5
nenadnesta <nenadnesta@gmail.com>
2019-11-18 15:01:31 +0100

    HomeComponent and Routing.

--- Biker/ClientApp/src/app/app.module.ts
+++ Biker/ClientApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
 import { NavMenuComponent } from './navmenu/navmenu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
