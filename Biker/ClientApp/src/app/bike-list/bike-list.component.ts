bb1da6e0ad0b104a1141ffc5d567adaf6d303691
nenadnesta <nenadnesta@gmail.com>
2019-11-27 19:59:01 +0100

    BikeListComponent - Client-side Filtering.

--- Biker/ClientApp/src/app/bike-list/bike-list.component.ts
+++ Biker/ClientApp/src/app/bike-list/bike-list.component.ts
@@ -7,6 +7,7 @@ import { Bike, KeyValuePair } from '../models/bike';
 })
 export class BikeListComponent implements OnInit {
   bikes: Bike[];
+  allBikes: Bike[];
   makes: KeyValuePair[];
   filter: any = {};
 
@@ -14,12 +15,26 @@ export class BikeListComponent implements OnInit {
 
   ngOnInit() {
     this.bikeService.getBikes()
-      .subscribe(bikes => this.bikes = bikes);
+      .subscribe(bikes => this.bikes = this.allBikes = bikes);
 
     this.bikeService.getMakes()
       .subscribe(makes => this.makes = makes);
   }
 
   onFilterChange() {
+    var bikes = this.allBikes;
+
+    if (this.filter.makeId)
+      bikes = bikes.filter(b => b.make.id == this.filter.makeId);
+
+    //if (this.filter.modelId)
+    //  bikes = bikes.filter(b => b.model.id == this.filter.modelId);
+
+    this.bikes = bikes;
+  }
+
+  resetFilter() {
+    this.filter = {};
+    this.onFilterChange();
   }
 } 
