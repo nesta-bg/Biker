23c32702c6542657dc7e1f31cc77a566ecab2075
nenadnesta <nenadnesta@gmail.com>
2019-12-02 19:51:44 +0100

    PhotosController - Adding Validation.

--- Biker/Controllers/PhotosController.cs
+++ Biker/Controllers/PhotosController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Biker.Controllers
@@ -14,6 +15,9 @@ namespace Biker.Controllers
     [Route("/api/bikes/{bikeId}/photos")]
     public class PhotosController : Controller
     {
+        private readonly int MAX_BYTES = 1 * 1024 * 1024;
+        private readonly string[] ACCEPTED_FILE_TYPES = new[] { ".jpg", ".jpeg", ".png" };
+
         private readonly IHostingEnvironment host;
         private readonly IBikeRepository repository;
         private readonly IUnitOfWork unitOfWork;
@@ -38,6 +42,11 @@ namespace Biker.Controllers
             var bike = await repository.GetBike(bikeId, includeRelated: false);
             if (bike == null)
                 return NotFound();
+
+            if (file == null) return BadRequest("Null file");
+            if (file.Length == 0) return BadRequest("Empty file");
+            if (file.Length > MAX_BYTES) return BadRequest("Max file size exceeded");
+            if (!ACCEPTED_FILE_TYPES.Any(s => s == Path.GetExtension(file.FileName))) return BadRequest("Invalid file type.");
 
             var uploadsFolderPath = Path.Combine(host.WebRootPath, "uploads");
 
