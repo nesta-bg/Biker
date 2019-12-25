8e60fa933bdc1ae9af9060ef421245e847e0d418
nenadnesta <nenadnesta@gmail.com>
2019-12-25 10:51:29 +0100

    ClientApp - Login, Logout.

--- Biker/ClientApp/src/app/navmenu/navmenu.component.ts
+++ Biker/ClientApp/src/app/navmenu/navmenu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'nav-menu',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+   
+  }
+
+  onLogout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/user/login']);
   }
 
 }
--- Biker/ClientApp/src/app/services/user.service.ts
+++ Biker/ClientApp/src/app/services/user.service.ts
@@ -22,4 +22,7 @@ export class UserService {
     return this.http.post(this.myAppUrl + 'api/AppUsers/Register', body);
   }
 
+  login(formData) {
+    return this.http.post(this.myAppUrl + 'api/AppUsers/Login', formData);
+  }
 }
